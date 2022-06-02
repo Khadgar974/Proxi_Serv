4aeac084faced252a0066532da7baa04a18d8a39
Samuel COTTEREAU <scottereau_ec@yahoo.fr>
2022-06-02 14:57:49 +0200

    rajout propriétés sur l'entité User, modifications sur formulaire

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -22,6 +22,7 @@ class RegistrationController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
+            $user->setCreatedAt(new \DateTimeImmutable());
             $user->setPassword(
             $userPasswordHasher->hashPassword(
                     $user,
