37a570ec765acee3a3cc59ef341084eb1e05b9a0
Samuel COTTEREAU <scottereau_ec@yahoo.fr>
2022-06-02 14:06:15 +0200

    Mise en place du premier formulaire inscription + création nav bar pour lien inscription

--- config/bundles.php
+++ config/bundles.php
@@ -7,9 +7,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-<<<<<<< HEAD
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-=======
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
->>>>>>> greg-create-user
 ];
