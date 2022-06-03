451b0a8e0814a1e2176a4bcc8571935680472333
Grégory MARINI <greg.marini@hotmail.fr>
2022-06-03 16:30:53 +0200

    création boutiqueFormType et layout commercant, création template commercantIndex et boutique_create. changement de nom du admninIndex.

--- src/Controller/BoutiqueController.php
+++ src/Controller/BoutiqueController.php
@@ -2,15 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Boutique;
+use App\Form\BoutiqueFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/boutiques')]
 class BoutiqueController extends AbstractController
 {
-    #[Route('/boutiques', name: 'app_boutiques')]
+    #[Route('/', name: 'app_boutiques')]
     public function index(): Response
-    {
+    {        
         return $this->render('boutique/boutiques_index.html.twig');
+    }
+    
+    #[Route('/create_boutique', name: 'app_create_boutique')]
+    public function createBoutique(): Response
+    {
+        $boutique = new Boutique;
+        $form = $this->createForm(BoutiqueFormType::class, $boutique);
+
+        return $this->render('boutique/boutique_create.html.twig');
     }
 }
