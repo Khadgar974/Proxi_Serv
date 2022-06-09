5851888bd6f64e0f44550762fbe8dbccd4811dd7
Samuel COTTEREAU <scottereau_ec@yahoo.fr>
2022-06-09 15:20:17 +0200

    Liaison entre l'index des produits et le détail des produits, différenciation page visu et page admin

--- src/Controller/commercant/CommercantProduitController.php
+++ src/Controller/commercant/CommercantProduitController.php
@@ -11,27 +11,38 @@ use App\Entity\Produits;
 use App\Form\ProduitsFormType;
 
 
+
 #[Route('/commercant')]
 class CommercantProduitController extends AbstractController
 {
-    #[Route('/produits/single_com/', name: 'app_single_com_produit')]
-    public function singleCom(): Response
+    #[Route('/produits/single_com/{id}', name: 'app_single_com_produit')]
+    public function singleCom($id, ProduitsRepository $produitsRepo): Response
     {
-        return $this->render('commercant/commercant_produit/produit_detail_com.html.twig', []);
+        $produit = $produitsRepo->findOneBy(['id' => $id]);
+        return $this->render('commercant/commercant_produit/produit_detail_com.html.twig', [
+            'produit' => $produit
+        ]);
     }
 
-    #[Route('/produits/single/', name: 'app_single_produit')]
-    public function single(): Response
+    #[Route('/produits/single/{id}', name: 'app_single_produit')]
+    public function single($id, ProduitsRepository $produitsRepo): Response
     {
+        $produit = $produitsRepo->findOneBy(['id' => $id]);
+        
+        
         return $this->render('produit/detail_produit.html.twig', [
-            
+            'produit' => $produit
         ]);
     }
 
     #[Route('/produits', name: 'app_commercant_produits_index')]
-    public function index(): Response
+    public function index(ProduitsRepository $produitsRepo): Response
     {
-        return $this->render('commercant/commercant_produit/produits_index.html.twig');
+        $produits = $produitsRepo->findAll();
+
+        return $this->render('commercant/commercant_produit/produits_index.html.twig',[
+            'produits' => $produits
+        ]);
     }
 
     #[Route('/produits/add', name: 'app_commercant_add_produit')]
