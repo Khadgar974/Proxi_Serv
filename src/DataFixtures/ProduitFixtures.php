190740b0c5785ab3786564146ca8708c4455b4f4
Samuel COTTEREAU <scottereau_ec@yahoo.fr>
2022-06-09 16:27:29 +0200

    maj fixtures produits

---
+++ src/DataFixtures/ProduitFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Produits;
+
+class ProduitFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $produit = new Produits();
+        $produit->setTitle('Calabraise');
+        $produit->setDescription('Une bien bonne pizza au Saumon');
+        $produit->setPrix('10');
+        $produit->setQuantite('5');
+        $produit->setReference('Cala');
+        $produit->setImage('pouet.jpg');
+        
+        $manager->persist($produit);
+        $manager->flush();
+    }
+}
