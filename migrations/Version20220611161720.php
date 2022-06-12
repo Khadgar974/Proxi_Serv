0559491565f808f99599eb5b2b388c2faf417c39
Thiébaut PINARDON <thiebaut_pinardon@yahoo.fr>
2022-06-12 21:20:25 +0200

    maj css

--- migrations/Version20220611161720.php
+++ migrations/Version20220611161720.php
@@ -10,9 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-
-final class Version20220611142820 extends AbstractMigration
-
+final class Version20220611161720 extends AbstractMigration
 {
     public function getDescription(): string
     {
