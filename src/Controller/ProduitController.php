b4485671a24ae8acfaf84cc050fdbd09b528e17f
Samuel COTTEREAU <scottereau_ec@yahoo.fr>
2022-06-14 09:46:39 +0200

    correction anomalie détails produits

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\ProduitsRepository;
+use App\Entity\Produits;
 
 class ProduitController extends AbstractController
 {
@@ -13,4 +16,17 @@ class ProduitController extends AbstractController
     {
         return $this->render('produit/bonplans.html.twig');
     }
+
+    // #[Route('/produits/single/{id}', name: 'app_single_produit')]
+    // public function single($id, ProduitsRepository $produitsRepo): Response
+    // {
+        
+    //     $produit = $produitsRepo->findOneBy(['id' => $id]);
+        
+
+    //     return $this->render('produit/detail_produit.html.twig', [
+    //         'produit' => $produit,
+            
+    //     ]);
+    // }
 }
