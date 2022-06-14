57c66bf4131e8ac11338f66f68a4f02cecfc5276
Grégory MARINI <greg.marini@hotmail.fr>
2022-06-14 11:22:32 +0200

    reset migration

--- migrations/Version20220614081151.php
+++ migrations/Version20220614081151.php
@@ -10,6 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
+final class Version20220614081151 extends AbstractMigration
 {
     public function getDescription(): string
     {
